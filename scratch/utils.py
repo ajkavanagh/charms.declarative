207cf9390c1ce414fac5a725fc6203181835e35d
Alex Kavanagh <alex@ajkavanagh.co.uk>
2017-10-16 21:06:42 +0100

    Moved KeyExists() to exceptions

--- scratch/utils.py
+++ scratch/utils.py
@@ -1,10 +1,4 @@
 
-# refactor this to a common utils module
-class KeyExists(Exception):
-    pass
-
-
-# TODO refactor this to a common set of utilities
 def maybe_format_key(key):
     """Format a key string to replace '-' with '_' so that they can be used
     as attributes.
