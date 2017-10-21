0b5d0e6427a4ca498386b6e03179efc31ed2378c
Alex Kavanagh <alex@ajkavanagh.co.uk>
2017-10-21 14:14:05 +0100

    Add utils.py with maybe_format_key() function

--- charms/declarative/core/utils.py
+++ charms/declarative/core/utils.py
@@ -1,13 +1,26 @@
+import keyword
 
-def maybe_format_key(key):
-    """Format a key string to replace '-' with '_' so that they can be used
-    as attributes.
+T = str.maketrans({'-': '_', '.': '_', '/': "__", '\\': "__"})
 
-    :param key: maybe a string.
+
+def maybe_format_key(name):
+    """Format a name into a key.  This is so that they can be used as legal
+    Python attributes.
+
+    A '-' or '.' becomes an '_'.
+    A '/' or `\` becomes '__'
+    Any other characters are illegal and will raise an AttributeError
+
+    :param key: maybe a string, if not, then it is just returned.
     :returns: string key
+    :raises: AttributeError for illegal characters.
     """
-    if isinstance(key, str):
-        key = key.replace("-", "_")
+    if not isinstance(name, str):
+        return name
+    key = name.translate(T)
+    if not key.isidentifier() or keyword.iskeyword(key):
+        raise AttributeError("name: {}, to key:{} is not a valid identifier"
+                             .format(name, key))
     return key
 
 
